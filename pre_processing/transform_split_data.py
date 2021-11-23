b3921fa1f2f1fc168961ff4988be4980907d6223
AndreiaSousa <andreia.sousa@wolterskluwer.com>
2021-11-23 17:28:25 +0000

    improvements to split data and transform images

--- pre_processing/transform_split_data.py
+++ pre_processing/transform_split_data.py
@@ -11,70 +11,88 @@ import torchvision.transforms as transforms
 data_dir = "/Users/andreiapfsousa/projects_andreiapfsousa/ComputerVisionProjects/videos_pilar/train"
 
 
-class LoadSplitTrainTest():
+class LoadSplitData:
     """split data to create the partitions to train, val and test."""
 
     def __call__(self, DATA_DIR: str, TEST_SPLIT: float):
         """Split data to create the partitions to train, val and test.
 
-            Parameters
-            ----------
-            DATA_DIR: str
-                path where are the images data to train.
-            TEST_SPLIT:
-                percentage to split the dataset.
-            """
+        Parameters
+        ----------
+        DATA_DIR: str
+            path where are the images data to train.
+        TEST_SPLIT:
+            percentage to split the dataset.
+        """
         data_transforms = transforms.Compose(
-                    [
-                        transforms.ToPILImage(),
-                        transforms.Resize(224),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                    ]
-                )
+            [
+                transforms.Resize((50, 50)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
-        dataset = datasets.ImageFolder(DATA_DIR, transform=data_transforms, target_transform=lambda x: x)
+        dataset = datasets.ImageFolder(
+            DATA_DIR, transform=data_transforms, target_transform=lambda x: x
+        )
+
         test_size = int(len(dataset) * TEST_SPLIT)
-        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset) - test_size, test_size], generator=torch.Generator().manual_seed(42))
+        train_dataset, test_dataset = torch.utils.data.random_split(
+            dataset,
+            [len(dataset) - test_size, test_size],
+            generator=torch.Generator().manual_seed(42),
+        )
 
         val_size = int(len(train_dataset) * TEST_SPLIT)
-        train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [len(train_dataset) - val_size, val_size], generator=torch.Generator().manual_seed(42))
+        train_dataset, val_dataset = torch.utils.data.random_split(
+            train_dataset,
+            [len(train_dataset) - val_size, val_size],
+            generator=torch.Generator().manual_seed(42),
+        )
 
-        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
-        valloader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True)
-        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=True)
+        trainloader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=4, shuffle=True
+        )
+        valloader = torch.utils.data.DataLoader(
+            val_dataset, batch_size=4, shuffle=True
+        )
+        testloader = torch.utils.data.DataLoader(
+            test_dataset, batch_size=4, shuffle=True
+        )
 
         return trainloader, testloader, valloader
 
     def imshow(self, img):
         """To show the images."""
-        classes = trainloader.dataset.classes
         img = img / 2 + 0.5  # unnormalize
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.show()
 
-        # get some random training images
-        dataiter = iter(trainloader)
-        images, labels = dataiter.next()
-
-        # show images
-        imshow(torchvision.utils.make_grid(images))
-        # print labels
-        print(" ".join("%5s" % classes[labels[j]] for j in range(4)))
-
 
 if __name__ == "__main__":
     DATA_DIR = "/Users/andreiapfsousa/projects_andreiapfsousa/ComputerVisionProjects/videos_pilar/train"
     TEST_SPLIT = 0.2
-    t = LoadSplitTrainTest()
-    trainloader, testloader, valloader = t(data_dir, TEST_SPLIT)
+    t = LoadSplitData()
+    trainloader, testloader, valloader = t(DATA_DIR, TEST_SPLIT)
 
     for sample, label in testloader:
         print("labels of testloader:", label)
+        print(sample.shape)
 
     for sample, label in trainloader:
         print("labels of trainloader:", label)
 
     for sample, label in valloader:
         print("labels of valloader:", label)
+
+    # see the images:
+    classes = ("deitada", "sentada")
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    t.imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(" ".join("%5s" % classes[labels[j]] for j in range(4)))
